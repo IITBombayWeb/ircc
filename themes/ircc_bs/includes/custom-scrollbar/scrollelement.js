5ecf3757848adaeefec102c8e0364754a9179bb1
nitesh624 <nsinha624@gmail.com>
2018-07-18 20:29:50 +0530

    first commit

---
+++ themes/ircc_bs/includes/custom-scrollbar/scrollelement.js
@@ -0,0 +1,8 @@
+(function($){
+    $(window).on("load",function(){
+        $(".field-expertise-faculty ul").mCustomScrollbar({
+            theme: "dark-thick",
+            scrollInertia: 600
+        });
+    });
+})(jQuery);
