7e395cdf3dd4e121b9471dd67d7d5eec4cfa74da
Aegir user <aegir@ircc.iitb.ac.in>
2018-08-06 10:53:43 +0530

    local.settings.php added to .gitignore

---
+++ local.settings.php
@@ -0,0 +1,12 @@
+<?php # local settings.php 
+ $databases['drona']['default'] = array (
+    'database' => 'deanfinal',
+    'username' => 'jobs',
+    'password' => 'intern#jb',
+    'prefix' => '',
+    'host' => '10.91.2.5',
+    'port' => '3306',
+    '_dsn_utf8_fallback' => TRUE,
+    'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+    'driver' => 'mysql',
+ );
