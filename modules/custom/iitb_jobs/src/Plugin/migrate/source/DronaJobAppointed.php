e16d1f3771d1cf06d7630b598514c4a1c8217206
tejasvaidya01 <tejasvaidya01@gmail.com>
2018-10-01 22:33:33 +0530

    Changed Appointed plugin.

--- modules/custom/iitb_jobs/src/Plugin/migrate/source/DronaJobAppointed.php
+++ modules/custom/iitb_jobs/src/Plugin/migrate/source/DronaJobAppointed.php
@@ -30,7 +30,7 @@ class DronaJobAppointed extends SourcePluginBase {
     $query = $con->select('RecruitmentSelectedCandidates', 'rsc');
     $query->innerJoin('RecruitmentDetails', 'rd', 'rsc.AdvCirNo = rd.AdvCirNo');
     $query->innerJoin('RecruitmentProjectDetails', 'rpd', 'rd.RecruitmentSrNo = rpd.RecruitmentSrNo AND (rsc.AdvJobCode = rpd.AdvJobCode OR rsc.AdvJobCode = rpd.ProCode)');
-    $query->innerJoin('RecruitmentDesignationDetails', 'rdd', 'rpd.RecruitmentSrNo = rdd.RecruitmentSrNo AND rpd.RecruitmentSrNo = rdd.RecruitmentSrNo AND rsc.DesigCode = rdd.DesgCode');
+    $query->innerJoin('RecruitmentDesignationDetails', 'rdd', 'rpd.RecruitmentSrNo = rdd.RecruitmentSrNo AND rpd.ProjectSrNo = rdd.ProjectSrNo AND rsc.DesigCode = rdd.DesgCode');
     // $query->condition('rd.RecruitmentSrNo', 'rpd.RecruitmentSrNo', "=");
     // $query->condition('rpd.RecruitmentSrNo', 'rdd.RecruitmentSrNo', "=");
     // $query->condition('rpd.ProjectSrNo', 'rdd.ProjectSrNo', "=");
