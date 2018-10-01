65b8df8b928e25583084b11c1482366880070f77
tejasvaidya01 <tejasvaidya01@gmail.com>
2018-10-01 22:37:23 +0530

    Changed Appointed plugin.

--- modules/custom/iitb_jobs/src/Plugin/migrate/source/DronaJobAppointed.php
+++ modules/custom/iitb_jobs/src/Plugin/migrate/source/DronaJobAppointed.php
@@ -29,11 +29,11 @@ class DronaJobAppointed extends SourcePluginBase {
 
     $query = $con->select('RecruitmentSelectedCandidates', 'rsc');
     $query->innerJoin('RecruitmentDetails', 'rd', 'rsc.AdvCirNo = rd.AdvCirNo');
-    $query->innerJoin('RecruitmentProjectDetails', 'rpd', 'rd.RecruitmentSrNo = rpd.RecruitmentSrNo AND (rsc.AdvJobCode = rpd.AdvJobCode OR rsc.AdvJobCode = rpd.ProCode)');
-    $query->innerJoin('RecruitmentDesignationDetails', 'rdd', 'rpd.RecruitmentSrNo = rdd.RecruitmentSrNo AND rpd.ProjectSrNo = rdd.ProjectSrNo AND rsc.DesigCode = rdd.DesgCode');
-    // $query->condition('rd.RecruitmentSrNo', 'rpd.RecruitmentSrNo', "=");
-    // $query->condition('rpd.RecruitmentSrNo', 'rdd.RecruitmentSrNo', "=");
-    // $query->condition('rpd.ProjectSrNo', 'rdd.ProjectSrNo', "=");
+    $query->innerJoin('RecruitmentProjectDetails', 'rpd', '(rsc.AdvJobCode = rpd.AdvJobCode OR rsc.AdvJobCode = rpd.ProCode)');
+    $query->innerJoin('RecruitmentDesignationDetails', 'rdd', 'rsc.DesigCode = rdd.DesgCode');
+    $query->condition('rd.RecruitmentSrNo', 'rpd.RecruitmentSrNo', "=");
+    $query->condition('rpd.RecruitmentSrNo', 'rdd.RecruitmentSrNo', "=");
+    $query->condition('rpd.ProjectSrNo', 'rdd.ProjectSrNo', "=");
     // $query->condition('rsc.DesigCode', 'rdd.DesgCode', "=");
     $query->fields('rdd',array('RecruitmentSrNo','ProjectSrNo','DesgSrNo'));
     $query->fields('rsc',array('AdvCirNo','AdvJobCode','DesigCode','Specialization','CandidateName','ApptSno','EmpCode','Status','EnteredDate','EnteredBy','ApprovalDate','ApprovedBy','Remarks'));
