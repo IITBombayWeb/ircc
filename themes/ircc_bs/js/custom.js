25be7d25c229d8676489dcec8e1a73dd7e449161
nitesh624 <nsinha624@gmail.com>
2019-01-17 12:48:46 +0530

    changes in theme

--- themes/ircc_bs/js/custom.js
+++ themes/ircc_bs/js/custom.js
@@ -50,10 +50,12 @@
 			if (previousTop - currentTop > scrollDelta) {
 				//if scrolling up...
 				mainHeader.removeClass('is-hidden');
+        $(".top-nav").removeClass('top-nav-fixed').addClass('top-nav-hidden');
 			}
 			else if( currentTop - previousTop > scrollDelta && currentTop > scrollOffset) {
 				//if scrolling down...
 				mainHeader.addClass('is-hidden');
+        $(".top-nav").removeClass('top-nav-hidden').addClass('top-nav-fixed');
         // $(".node-type-link a").css("z-index", "20");
 
 			}
